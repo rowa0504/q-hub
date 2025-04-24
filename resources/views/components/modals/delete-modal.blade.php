5ea4c1332ea041dfccc6b6088b8e771a1ceb01ee
rowa0504 <shun.tg1212@gmail.com>
2025-04-24 16:48:52 +0900

    delte, edit, report modal

---
+++ resources/views/components/modals/delete-modal.blade.php
@@ -0,0 +1,22 @@
+{{-- デリートモーダル --}}
+<div class="modal fade" id="deleteModal-{{ $post->id }}" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Confirm Deletion</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+          You want to delete this post?
+        </div>
+        <div class="modal-footer">
+          <form action="#" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Yes, Delete</button>
+          </form>
+          <button class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
