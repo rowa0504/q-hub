64bbf564d69dbdf8e1895002513ef6c6b88bbfa2
yuki <yuki@example.com>
2025-04-25 18:02:46 +0900

    profilecontroller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,41 +9,41 @@ class ProfileController extends Controller
 {
     private $user;
 
-    public function __construct(User $user,){
-        $this->User = $user;
+    public function __construct(User $user){
+        $this->user = $user;
     }
  
     public function show($id){
         $user = $this->user->findOrFail($id);
-        return view('profile.show')->with('user',$user);
+        return view('profile.index')->with('user', $user);
     }
 
-    public function edit() {
-        $user = $this->user->findOrFail(Auth::user()->id);
-        return view('profile.show')->with('user',$user);
-    }
+    // public function edit() {
+    //     $user = $this->user->findOrFail(Auth::user()->id);
+    //     return view('profile.show')->with('user',$user);
+    // }
 
-    public function update(Request $request, $id){
-        $request->validate([
-            'name'         => 'required|max:20',
-            'email'        => 'required|max:50',
-            'introduction' => 'required|min:1|max:1000',
-            'avatar'       => 'mimes:jpeg,jpg,png,gif|max:1048'
-        ]);
+    // public function update(Request $request, $id){
+    //     $request->validate([
+    //         'name'         => 'required|max:20',
+    //         'email'        => 'required|max:50',
+    //         'introduction' => 'required|min:1|max:1000',
+    //         'avatar'       => 'mimes:jpeg,jpg,png,gif|max:1048'
+    //     ]);
 
-        $user = $this->user->findOrFail($id);
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->introduction = $request->introduction;
+    //     $user = $this->user->findOrFail($id);
+    //     $user->name = $request->name;
+    //     $user->email = $request->email;
+    //     $user->introduction = $request->introduction;
     
-        if ($request->avatar){
-            $user->avatar = 'data:image/' . $request->avatar->extension() .
-            ';base64,'. base64_encode(file_get_contents($request->avatar));
-        }
+    //     if ($request->avatar){
+    //         $user->avatar = 'data:image/' . $request->avatar->extension() .
+    //         ';base64,'. base64_encode(file_get_contents($request->avatar));
+    //     }
     
-        $user->save();
-        return redirect()->back();
-    }
+    //     $user->save();
+    //     return redirect()->back();
+    // }
     
     
 }
