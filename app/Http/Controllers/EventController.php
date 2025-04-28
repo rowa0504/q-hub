f59923fcbb4f849a67fd0e0da7cb8242d0f5ee3e
Masahiro Sakakibara <masahiro@example.com>
2025-04-28 17:29:27 +0800

    calendar

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -19,22 +19,4 @@ class EventController extends Controller
     {
         return view('posts.categories.events.show');
     }
-
-    public function indexCalendar(){
-        // イベントデータを取得（適宜変更）
-        $events = Post::where('category_id', 1)->get();
-
-        // FullCalendarが期待する形式に変換して返す
-        $formattedEvents = $events->map(function ($event) {
-            return [
-                'id' => $event->id,
-                'title' => $event->title,
-                'start' => $event->startdatetime->toIso8601String(), // 開始日時をISO 8601形式に変換
-                'end' => $event->enddatetime ? $event->enddatetime->toIso8601String() : null, // 終了日時もISO 8601形式に変換
-            ];
-        });
-
-        return response()->json($formattedEvents);
-    }
-
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventCalendarController;
 
 Route::middleware('api')->group(function () {
-    Route::get('/events', [EventController::class, 'indexCalendar']);
+    Route::get('/events', [EventCalendarController::class, 'index']);
     // Route::post('/events', [EventController::class, 'store']);
 });
