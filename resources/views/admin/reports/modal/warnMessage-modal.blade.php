95b85fb6c450653f7d3155d6a2ded4cacc45c286
Masahiro Sakakibara <masahiro@example.com>
2025-05-14 15:54:20 +0800

    #

--- resources/views/admin/reports/modal/warnMessage-modal.blade.php
+++ resources/views/admin/reports/modal/warnMessage-modal.blade.php
@@ -13,23 +13,29 @@
             @csrf
             @method('PATCH')
 
-        <div class="modal-body">
-            <div class="mb-3 mt-3">
-                <label class="form-label fw-bold">Message</label>
-                <textarea name="message" class="form-control" rows="4" required placeholder="Write a custom warning message...">{{ old('message', $report->message) }}</textarea>
+            <div class="modal-body">
+                <div class="mb-3 mt-3">
+                    <label class="form-label fw-bold">Message</label>
+                    <textarea name="message" class="form-control" rows="4" required placeholder="Write a custom warning message...">{{ old('message', $report->message) }}</textarea>
+                </div>
+                @error('message')
+                    <p class="text-danger small">{{ $message }}</p>
+                @enderror
             </div>
-            @error('message')
-                <p class="text-danger small">{{ $message }}</p>
-            @enderror
-        </div>
 
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-            <button type="submit" class="btn btn-warning">
-              <i class="fa-solid fa-paper-plane"></i> Edit Message
-            </button>
-          </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="submit" class="btn btn-warning">
+                <i class="fa-solid fa-paper-plane"></i> Edit Message
+                </button>
         </form>
+            <form action="{{ route('admin.deleteReportMessage', $report->id) }}" method="post">
+                @csrf
+                @method('PATCH')
+
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+          </div>
       </div>
     </div>
 </div>
